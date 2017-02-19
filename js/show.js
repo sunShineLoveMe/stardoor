d1545f1f7763e67c7ecaf7d102b2931725feaeb8
june <june@MacBook-Pro-2.local>
2017-02-19 22:14:38 +0800

    轮播图

--- js/show.js
+++ js/show.js
@@ -1,4 +1,36 @@
+var t = n = 0,count;
 jQuery(document).ready(function($) {
+	count = $("#banner_list a").length;
+	$("#banner_list a:not(:first-child)").hide();
+	$("#banner_info").html($("#banner_list a:first-child").find("img").attr('alt'));
+	$("#banner_info").click(function() {
+		window.open($("#banner_list a:first-child").attr('href'), "_blank")
+	});
+	$("#banner li").click(function() {
+		var i = $(this).text() - 1; //获取Li元素内的值，即1，2，3，4
+		n = i;
+		if (i >= count) return;
+		$("#banner_info").html($("#banner_list a").eq(i).find("img").attr('alt'));
+		
+		$("#banner_info").unbind().click(function() {
+			window.open($("#banner_list a").eq(i).attr('href'), "_blank")
+		});
+		 $("#banner_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
+		
+		document.getElementById("banner").style.background = "";
+		$(this).toggleClass("on");
+		$(this).siblings().removeAttr("class");
+	});
+
+	t = setInterval("showAuto()", 4000);
+	$("#banner").hover(function() {
+			clearInterval(t)
+		},
+		function() {
+			t = setInterval("showAuto()", 4000);
+		});
+
+	
 	$(".scroll").click(function(event) {
 		event.preventDefault();
 		$('html,body').animate({
@@ -28,3 +60,8 @@ jQuery(document).ready(function($) {
 		}
 	});
 });
+
+function showAuto() {
+		n = n >= (count - 1) ? 0 : ++n;
+		$("#banner li").eq(n).trigger('click');
+	}
