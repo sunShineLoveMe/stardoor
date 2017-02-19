2cd584af72df45a1ec53f08b229f59151ab091ae
june <june@MacBook-Pro-2.local>
2017-02-19 20:44:54 +0800

    锚点定位

---
+++ js/about.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+ $(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+	});
+ $('a[href*=#]').click(function(){
+ 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	  var $target = $(this.hash);
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top - 55;
+        $('html,body').animate({
+          scrollTop: targetOffset
+        },1000);
+        return false;
+      }
+ 	}
+ });
+});
