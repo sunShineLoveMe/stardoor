17dfb136f5d80ee3bc64a4915f25944bc19a9859
june <june@MacBook-Pro-2.local>
2017-02-20 23:11:59 +0800

    maodian dingwie

--- js/new.js
+++ js/new.js
@@ -5,4 +5,26 @@ jQuery(document).ready(function($) {
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
       });
+ $('a[href*=#]').click(function() {
+ 	// $(".new-panel span[class='flag-icon']").each(function(){
+ 	// 	console.log($(this));
+ 	// });
+ 	$(".flag-icon").each(function(){
+ 		$(this).removeClass("flag-icon");
+ 	});
+ 	$(this).find('span').addClass("flag-icon");
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top - 55;
+        $('html,body').animate({
+          scrollTop: targetOffset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+
 });
