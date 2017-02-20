aa78a76d67e4a24a7452065512c78abb96ffae8e
june <june@MacBook-Pro-2.local>
2017-02-20 21:51:51 +0800

    首页滚动条禁止

--- js/home.js
+++ js/home.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
+	 var window_height = $(window).height() - 45;
+	 var windwo_width = $(window).width();
+	 $(".home-bg").css("width", windwo_width).css("height", window_height);
+	 $('.dropdown-toggle').dropdown();
 	 $('.tooltip').tooltipster({
 	 	interactive: true,
 	 });
-
 });
