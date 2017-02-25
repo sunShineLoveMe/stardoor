f694ff2d8c74d584b1708587a858d9e88562beba
june <june@MacBook-Pro-2.local>
2017-02-25 21:35:38 +0800

    首页滚动

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	 var window_height = $(window).height() - 45;
-	 var windwo_width = $(window).width() + 45;
+	 var windwo_width = $(window).width();
 	 $(".home-bg").css("width", windwo_width).css("height", window_height);
 	 $('.dropdown-toggle').dropdown();
 	 $('.tooltip').tooltipster({
